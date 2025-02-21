8d5e37bf23f7349f18b4b31fa2a60e0b6415eb48
Ricardo García <ricardogarciajarillo@gmail.com>
2025-02-21 19:04:21 +0100

    fix: favorites and create purposals

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,21 +1,21 @@
-import { useContext } from "react"
-import { AuthContext } from "../../contexts/AuthContext"
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
+const Navbar = () => {
+  const { logout } = useContext(AuthContext);
 
+  return (
+    <>
+      <button
+        className="btn btn-danger"
+        onClick={() => {
+          logout();
+        }}
+      >
+        Logout
+      </button>
+    </>
+  );
+};
 
-const Navbar = () =>{
-    const {logout } = useContext(AuthContext)
-
-    return(
-        <>
-         <button className="btn btn-danger" onClick={() => {
-                  logout()
-
-                }}>Logout</button>
-
-                
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
--- src/services/purposal.service.js
+++ src/services/purposal.service.js
@@ -9,3 +9,5 @@ export const getPurposal = (id) => authenticatedHttp.get(`/purposals/${id}`)
 export const editPurposal = (id, purposal) => authenticatedHttp.patch(`/purposals/${id}`,purposal)
 
 export const deletePurposal = (id) => authenticatedHttp.delete(`/purposals/${id}`)
+
+export const getPurposals = () => authenticatedHttp.get("/purposals/promoter");
