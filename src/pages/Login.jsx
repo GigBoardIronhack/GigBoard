f7cfb3728ef31525b6133a79b6dce74b41a67297
Ricardo García <ricardogarciajarillo@gmail.com>
2025-03-02 23:53:08 +0100

    feat: register and login styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,70 +2,79 @@ import { useState, useContext } from "react";
 import { loginService } from "../services/auth.service";
 import { AuthContext } from "../contexts/AuthContext";
 
-
 const Login = () => {
-
-  const [formData, setFormData]= useState({
+  const [formData, setFormData] = useState({
     email: "",
     password: ""
-  })
-  const [error, setError] = useState(null)
+  });
+  const [error, setError] = useState(null);
   const { login, isAuthLoaded, currentUser } = useContext(AuthContext);
 
-  const handleChange = (e) =>{
-    const {name, value}= e.target
-    setFormData((prevState)=>({
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
       ...prevState,
       [name]: value
-    }))
-  }
+    }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-      const loginUser = await loginService(formData)
-      login(loginUser.accessToken)
-      
-
-     
-    }catch(error){
-      setError(error.message)
+    try {
+      const loginUser = await loginService(formData);
+      login(loginUser.accessToken);
+    } catch (error) {
+      setError(error.message);
     }
-  }
+  };
+
   if (!isAuthLoaded) {
-    return 'loading'
+    return "loading";
   }
 
   if (isAuthLoaded && currentUser) {
-    return 'Ya estas logueado'
+    return "Ya estas logueado";
   }
+
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-      <div >
-        <label htmlFor="email">
-          <input type="text"
-           id="email"
-           name="email"
-             onChange={handleChange}/>
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          <input type="password"
-           id="password" 
-           name="password"         
-           onChange={handleChange}/>
-        </label>
-      </div>
-      <button type="submit">Login</button>
-      {error && <div className="alert alert-danger" role="alert">
-          {error}
-        </div>}
-  
+    <div className="bg-white dark:bg-[#101C29] min-h-screen flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-[#004e64] p-6 rounded-lg shadow-lg w-full max-w-md">
+        <div>
+          <h1 className="text-2xl font-semibold text-white mb-4 text-center">Bienvenido a GigBoard</h1>
+          <label htmlFor="email" className="block mb-2">
+            <input
+              type="text"
+              id="email"
+              name="email"
+              onChange={handleChange}
+              className="w-full p-2 border border-[#d76a03] rounded text-gray-400"
+              placeholder="email"
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="password" className="block mb-2">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleChange}
+              className="w-full p-2 border border-[#d76a03] rounded text-gray-400"
+              placeholder="password"
+            />
+          </label>
+        </div>
+        <button type="submit" className="w-full p-2 bg-[#d76a03] text-white rounded hover:bg-[#e3b505]">
+          Login
+        </button>
+        {error && (
+          <div className="alert alert-danger mt-2 text-[#e3b505]" role="alert">
+            {error}
+          </div>
+        )}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
