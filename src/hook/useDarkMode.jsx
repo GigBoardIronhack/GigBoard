38a21897552e4469ad87314ffd201fda8ee39b30
Juan Manuel <j-grafi@hotmail.com>
2025-03-28 17:35:45 +0100

    fix dark mode

--- src/hook/useDarkMode.jsx
+++ src/hook/useDarkMode.jsx
@@ -1,10 +1,17 @@
 import { useEffect } from "react";
 
 const useDarkMode = () => {
+  // Desativo modo oscuro por el momento
+  const enableDarkMode = false;
+  
   useEffect(() => {
+    if (!enableDarkMode) return; 
     const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
     const applyTheme = () => {
+       // Desativo modo oscuro por el momento
+     
+
       if (mediaQuery.matches) {
         document.documentElement.classList.add("dark");
       } else {
